22d97213e458271caf41bbc4946befbd29347a10
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-21 12:17:48 -0500

    Starting Problem 3

---
+++ Py-Solutions/Problem-3.py
@@ -0,0 +1,11 @@
+# What is the largest prime factor of the number 600851475143?
+import math
+
+def isPrime(num):
+  return sum([i for i in range(1, int(math.sqrt(num)) + 1) if num % i == 0]) == 1
+
+def largestPrimeFactor(num):
+  return [i for i in range(2, int(math.sqrt(num)) + 1) if num % i == 0 and isPrime(i)][-1]
+    
+answer = largestPrimeFactor(600851475143)
+print(answer)
