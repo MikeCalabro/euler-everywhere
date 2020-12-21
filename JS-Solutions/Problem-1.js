c1308d6ee1b59c7670476d8027371d4abfa9342f
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-21 10:53:21 -0500

    Problem 1 Done

--- JS-Solutions/Problem-1.js
+++ JS-Solutions/Problem-1.js
@@ -1,1 +1,14 @@
-console.log('Poop');
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+const three_or_five = (limit) => {
+  answer = 0;
+  for(i=3;i<limit;i++){
+    if(i % 3 == 0 | i % 5 == 0){
+      answer += i;
+    }
+  }
+  return(answer);
+}
+
+console.log(three_or_five(1000));
--- R-Solutions/Problem-1.r
+++ R-Solutions/Problem-1.r
@@ -1,1 +1,14 @@
-print("Hello Code Runner")
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+three_or_five <- function(limit){
+  answer = 0;
+  for(i in 1:limit-1){
+    if(i %% 3 == 0 | i %% 5 == 0){
+      answer <- answer + i
+    }
+  }
+  return(answer)
+}
+
+print(three_or_five(1000))
