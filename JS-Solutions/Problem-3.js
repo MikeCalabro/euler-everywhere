90cc8028cceb1844d590417a8751832b12187c16
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-21 21:52:35 -0500

    Don't forget the let in for loops in JS

--- JS-Solutions/Problem-3.js
+++ JS-Solutions/Problem-3.js
@@ -1,2 +1,23 @@
 // What is the largest prime factor of the number 600851475143?
 
+const isPrime = (num) => {
+  for(let i=2;i<=Math.sqrt(num)+2;i++){
+    if(num % i == 0){
+      return(false);
+    }
+  }
+  return(true);
+}
+
+const largestPrimeFactor = (num) => {
+  let largest = 0;
+  for(let i=2;i<=Math.sqrt(num)+2;i++){
+    if(num % i == 0 & isPrime(i)){
+      largest = i;
+    }
+  }
+  return(largest);
+}
+
+
+console.log(largestPrimeFactor(600851475143));
