9b4c32a0bf7faf54ea4f783ed0249d43ad763c49
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-21 23:24:49 -0500

    Got answer, need to functionalize

--- C-Solutions/Problem-2.c
+++ C-Solutions/Problem-2.c
@@ -1,3 +1,22 @@
 /*
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 */
+#include <stdio.h>
+
+
+
+int main(){
+  int sum = 0;
+  int arrLen = 32;
+  int arr[32] = {1, 2};
+  for(int i = 2; i < arrLen; i++) {
+    arr[i] = arr[i-1] + arr[i-2];
+  }
+  for(int i = 0; i < arrLen; i++) {
+    if(arr[i] % 2 == 0) {
+      sum += arr[i];
+    }
+  }
+  printf("%d\n", sum);
+  return 0;
+}
