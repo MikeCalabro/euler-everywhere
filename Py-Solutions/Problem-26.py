01bdb178bc1fe4d23ad9a833df712cb50bd7219f
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-23 13:41:23 -0500

    Problem 26 Complete

---
+++ Py-Solutions/Problem-26.py
@@ -0,0 +1,23 @@
+import decimal
+import math
+
+decimal.getcontext().prec = 2000
+
+max_j = 0
+max_i = 0
+
+for i in range(1,1000):
+  string = str(decimal.Decimal(1)/decimal.Decimal(i) * 1000000000000000000000 % 1)[2:]
+  test_str = str(decimal.Decimal(1)/decimal.Decimal(i) * 1000000000000000000000 % 1)[2:30]
+  for j in range(1,len(string)-25):
+    if test_str == string[j:j+28]:
+      if j > max_j:
+        max_j = j
+        max_i = i
+      break
+
+def main():
+  print(max_i, max_j)
+
+if __name__ == "__main__":
+    main()
