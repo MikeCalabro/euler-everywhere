e4416546c1b5cf6fea0e9ac62c6d457693e4fcf0
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-24 11:40:40 -0500

    Got the answer, but sloppily

--- Py-Solutions/Problem-27.py
+++ Py-Solutions/Problem-27.py
@@ -5,8 +5,8 @@ import math
 def isPrime(num):
   return sum([i for i in range(1, int(math.sqrt(num)) + 1) if num % i == 0]) == 1
 
-b_list = [i for i in range(2,43) if isPrime(i)]
-a_list = [i for i in range(-43,44, 2)]
+b_list = [i for i in range(2,1000) if isPrime(i)]
+a_list = [i for i in range(-999,1000, 2)]
 
 ab_list = []
 for a in a_list:
@@ -16,13 +16,10 @@ for a in a_list:
 n = 1
 while(len(ab_list) > 1):
   for i in ab_list:
-    if n**2 + n*i[0] + i[1] < 2 or not isPrime(n**2 + n*i[0] + i[1]):
+    if n**2 + n*i[0] + i[1] < 2: 
+      ab_list.remove(i)
+    elif not isPrime(n**2 + n*i[0] + i[1]):
       ab_list.remove(i)
   n += 1
-
-print(ab_list)
-print(ab_list[0][0]*ab_list[0][1])
-print(n-1)
-
-for n in range(45):
-  print(isPrime(n**2 + -5*n +47))
+  print(ab_list)
+  print(n)
