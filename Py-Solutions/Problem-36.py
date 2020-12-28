7f2ccdd40f1410f50926b342d6d7b66ef4e8619d
Michael Calabro <michael_calabro@college.harvard.edu>
2020-12-28 11:06:21 -0500

    Problem 36 Done

--- Py-Solutions/Problem-36.py
+++ Py-Solutions/Problem-36.py
@@ -1,8 +1,35 @@
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
+def decToBinary(num):
+    if num == 1:
+        return "1"
+    stringNum = ""
+    while num > 1:
+        if num % 2 == 1:
+            stringNum = "1" + stringNum
+        else:
+            stringNum = "0" + stringNum
+        num = num // 2
+    stringNum = "1" + stringNum
+    return stringNum
 
-def DecimalToBinary(num): 
-    if num > 1: 
-        DecimalToBinary(num // 2) 
-    print(num % 2)
+def isPalindrome(str_num):
+  rev_num = str_num[-1]
+  for i in range(len(str_num)-2, -1, -1):
+    rev_num += str_num[i]
+  if rev_num == str_num:
+    return True
+  return False
 
-print(DecimalToBinary(8))
+def twoBasePal(limit):
+    final_sum = 0
+    for i in range(1,limit):
+        if isPalindrome(str(i)) and isPalindrome(decToBinary(i)):
+            final_sum += i
+    return final_sum
+
+def main():
+    print(twoBasePal(1000000))
+
+if __name__ == "__main__":
+    main()
