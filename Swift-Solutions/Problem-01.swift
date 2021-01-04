6bc60b44e600e573e384331bc15b452e4449100f
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-04 16:06:46 -0500

    Added Explicit typing... for better performance?

--- Swift-Solutions/Problem-01.swift
+++ Swift-Solutions/Problem-01.swift
@@ -1,7 +1,7 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 func threeOrFive(limit: Int) -> Int {
-  var sum = 0
+  var sum: Int = 0
   for i in 1...limit-1 {
     if i % 3 == 0 || i % 5 == 0 {
       sum += i
