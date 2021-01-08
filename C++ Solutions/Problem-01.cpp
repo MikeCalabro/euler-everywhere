5e5298cca666fd865d9f0855416dd53b8dea09a9
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-08 15:26:33 -0500

    C++ because why not! Problem 1 exactly like C

---
+++ C++ Solutions/Problem-01.cpp
@@ -0,0 +1,20 @@
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000. */
+#include <iostream>
+
+int threesAndFives(int limit);
+
+int main(void) {
+  int answer = threesAndFives(1000);
+  std::cout << answer;
+}
+
+int threesAndFives(int limit) {
+  int sum = 0;
+  for (int i=0;i<limit;i++) {
+    if (i % 3 == 0 | i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
