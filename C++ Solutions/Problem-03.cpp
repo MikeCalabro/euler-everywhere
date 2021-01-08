18ee82e0bd94dc7b0a90b206629560b0dc604f0d
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-08 16:20:49 -0500

    Works for smaller numbers, but real number too large

---
+++ C++ Solutions/Problem-03.cpp
@@ -0,0 +1,37 @@
+/* What is the largest prime factor of the number 600851475143? */
+#include <iostream>
+using namespace std;
+
+bool isPrime(double num);
+int largestPrimeFactor(double num);
+
+int main(void) {
+  int x = largestPrimeFactor(13195);
+  cout << x;
+}
+
+bool isPrime(double num) {
+  if (num <= 1) {
+    return false;
+  }
+  if (num == 2) {
+    return true;
+  }
+  for (int i=2;i<int(sqrt(num))+1;i++) {
+    if (int(num) % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int largestPrimeFactor(double num) {
+  int intNum = int(num);
+  int largest = 0;
+  for (int i=2;i<int(sqrt(num))+1;i++) {
+    if ((intNum % i == 0) & (isPrime(i))) {
+      largest = i;
+    }
+  }
+  return largest;
+}
