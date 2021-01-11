357402df6756d1a81017f22f09a9cc2630d522c2
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-11 10:19:22 -0500

    Problem 2 C better

--- C-Solutions/Problem-02.c
+++ C-Solutions/Problem-02.c
@@ -1,31 +1,30 @@
 /* By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. */
 #include <stdio.h>
+#include <time.h>
 
-int main(){
-  /* Initialize the limit (largest possible fib number) and sum (will eventually be our answer) */
-  int limit = 4000000;
-  int sum = 0;
+int evenFibSum(int limit);
 
-  /* Populate the array of Fibonacci Numbers */
-  int arr[50] = {1, 2};
-  for(int i = 2; i < 50; i++) {
-    arr[i] = arr[i-1] + arr[i-2];
-    if(arr[i] + arr[i-1] > limit) {
-      break;
-    }
-  }
+int main(void) {
+  clock_t begin = clock();
 
-  /* Calculate length of populated array (there are 0's on the end because I initialized with a number too big i.e 50) */ 
-  int arrLen = sizeof(arr) / sizeof(arr[0]);
+  int answer = evenFibSum(4000000);
+  printf("%d\n", answer);
 
-  /* Loop through the array, adding the even numbered terms to sum (which will be the final answer) */
-  for(int i = 0; i < arrLen; i++) {
-    if(arr[i] % 2 == 0) {
-      sum += arr[i];
-    }
-  }
-
-  /* Print the final answer */
-  printf("%d\n", sum);
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("RunTime: %f\n", time_spent);
   return 0;
+}
+
+int evenFibSum(int limit) {
+  int last = 2, secondLast = 1, evenSum = 0, placeHolder;
+  while (last < limit) {
+    if (last % 2 == 0) {
+      evenSum += last;
+    }
+    placeHolder = last;
+    last = last + secondLast;
+    secondLast = placeHolder;
+  }
+  return evenSum;
 }
