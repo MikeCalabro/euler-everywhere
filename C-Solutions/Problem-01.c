6a719b4a94ea9e9c595a3958855f378e9e7ed19a
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-11 10:13:47 -0500

    Renaming C Files

--- C-Solutions/Problem-01.c
+++ C-Solutions/Problem-01.c
@@ -4,18 +4,26 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
 #include <stdio.h>
+#include <time.h>
 
 int threeOrFive(int limit);
 
+
 int main() {
+  clock_t begin = clock();
+
   int answer = threeOrFive(1000);
   printf("%d\n", answer);
+  
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("RunTime: %f\n", time_spent);
   return 0;
 }
 
 int threeOrFive(int limit) {
   int sum = 0;
-  for(int i = 1; i < limit; i++){
+  for(int i=1; i<limit; i++){
     if(i % 3 == 0 | i % 5 == 0){
       sum = sum + i;
     }
