1840b725ed79af550833d8a3b95b2750e170d277
Michael Calabro <michael_calabro@college.harvard.edu>
2021-01-11 21:34:33 -0500

    Problem 6 C++ Complete

---
+++ C++ Solutions/Problem-06.cpp
@@ -0,0 +1,35 @@
+/* # Find the difference between the sum of the squares of the first 100 natural numbers and the square of the sum. */
+
+#include <iostream>
+using namespace std;
+
+int sumOfSquares(int limit);
+int squareOfSums(int limit);
+int squareMinusSum(int limit);
+
+int main() {
+  int answer = squareMinusSum(100);
+  cout << answer;
+}
+
+int sumOfSquares(int limit) {
+  int sum = 0;
+  for (int i=1; i<=limit; i++) {
+    sum += (i*i);
+  }
+  return sum;
+}
+
+int squareOfSums(int limit) {
+  int sum = 0;
+  for (int i=1; i<=limit; i++) {
+    sum += i;
+  }
+  int square = sum * sum;
+  return square;
+}
+
+int squareMinusSum(int limit) {
+  int difference = squareOfSums(limit) - sumOfSquares(limit);
+  return difference;
+}
